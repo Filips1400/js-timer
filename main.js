52752fb9a1b9edfe09bed39031adce64c21b4d25
JamesObry <59664773+JamesObry@users.noreply.github.com>
2020-11-12 17:32:45 +0200

    second commit
    i did js timer

---
+++ main.js
@@ -0,0 +1,20 @@
+let start = 1;
+function timerstart() {
+    if (start == 1) {
+        let time = prompt('Please, enter seconds');
+        const countDownEl = document.getElementById("countdown");
+
+        setInterval(updateCountdown, 1000);
+        
+        function updateCountdown() {
+          const minutes = Math.floor(time / 60);
+          let seconds = time % 60;
+          seconds = seconds < 10 ? "0" + seconds: seconds;
+          countDownEl.innerHTML = `${minutes}:${seconds}`;
+          start = false;
+          if (seconds > 0 || minutes > 0) {
+            time--;
+          }
+        }
+    }
+}
